657016a30a9f8456fc00e14ff0499fd9af3c3dca
范洪月 <fanhongyue@zhongan.io>
2018-10-26 11:03:02 +0800

    asd

--- src/models/okex/okex_future_ticker.go
+++ src/models/okex/okex_future_ticker.go
@@ -6,21 +6,21 @@ type ReqFurtureTicker struct {
 }
 
 type RspFurtureTicker struct {
-	Data    *FurtureTicker `json:"data"`
-	Channel string         `json:"channel"`
-	Binary  int32          `json:"binary"`
+	Data    FurtureTicker `json:"data"`
+	Channel string        `json:"channel"`
+	Binary  int32         `json:"binary"`
 }
 
 type FurtureTicker struct {
-	LimitHigh  string  `json:"limitHigh"`
-	Vol        int32   `json:"vol"`
-	Last       float32 `json:"last"`
-	Sell       float32 `json:"sell"`
-	Buy        float32 `json:"buy"`
-	UnitAmount int32   `json:"unitAmount"`
-	HoldAmount int32   `json:"hold_amount"`
-	ContractId int64   `json:"contractId"`
-	High       float32 `json:"high"`
-	Low        float32 `json:"low"`
-	LimitLow   string  `json:"limitLow"`
+	LimitHigh  string `json:"limitHigh"`
+	Vol        string `json:"vol"`
+	Last       string `json:"last"`
+	Sell       string `json:"sell"`
+	Buy        string `json:"buy"`
+	UnitAmount string `json:"unitAmount"`
+	HoldAmount string `json:"hold_amount"`
+	ContractId int64  `json:"contractId"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	LimitLow   string `json:"limitLow"`
 }
